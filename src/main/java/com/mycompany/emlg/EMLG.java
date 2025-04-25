2c07745d7a8d632e4b2f2308e3523c7652a4e7d4
Mateo Lugmaña <mateodrake@hotmail.com>
2025-04-25 00:47:28 -0500

    Primer comentario Local

---
+++ src/main/java/com/mycompany/emlg/EMLG.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.emlg;
+
+/**
+ *
+ * @author mateo
+ */
+public class EMLG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hola primer repositorio");
+    }
+    
+}
